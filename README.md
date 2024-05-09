844b52228e8bf7916030eca9d60be0a88e0349c3
daniyalismail <daniyalismail19@gmail.com>
2024-05-09 22:35:48 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ToDoList
+ A Program Which Allows You To Input Your (Habit Or Any Reminder Or Anything) And It Will Print Your List
---
+++ main.ts
@@ -0,0 +1,19 @@
+import inquirer from "inquirer";
+let todo = [];
+let todoQuestions;
+do{
+     todoQuestions = await inquirer.prompt([
+  {
+    name: "ListElements",
+    type: "input",
+    message: "What Do You Want To Add In Your List = ",
+  },
+  {
+    message: "Wanna Add More Int The List (Yes Or No) = ",
+    type: "confirm",
+    name: "AddConfirm",
+  },
+]);
+todo.push(todoQuestions.ListElements);
+}while(todoQuestions.AddConfirm == true);
+console.log(todo);
